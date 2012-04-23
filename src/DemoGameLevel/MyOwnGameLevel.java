7283558ca3273528a1c6a30467ffe1ee42c09716
Chenbo Zhu <cz45@duke.edu>
2012-04-23 19:47:13 -0400

    modified

---
+++ src/DemoGameLevel/MyOwnGameLevel.java
@@ -0,0 +1,48 @@
+package DemoGameLevel;
+
+import game.TopDownGameEngine;
+import gameLevel.GameLevel;
+
+import java.awt.Graphics2D;
+import java.util.List;
+
+import load.Load;
+
+import state.DefaultLastLevelState;
+import collisionSystem.EnemyBulletCollision;
+
+public class MyOwnGameLevel extends GameLevel {
+    
+	public List<List<Object>> list = Load.list;
+	
+	public MyOwnGameLevel(TopDownGameEngine parent) {
+		super(parent);
+		myState = new DefaultLastLevelState(parent, this);
+	}
+
+	public void initResources() {
+
+	}
+
+	public void innerStateUpdate(long elapsedTime) {
+
+	}
+
+	public void render(Graphics2D g) {
+		playfield.render(g);
+		gameRender(g);
+
+	}
+
+	public void gameRender(Graphics2D g) {
+		fontManager.getFont("FPS Font").drawString(g, levelRequirement, 20, 15);
+		fontManager
+				.getFont("FPS Font")
+				.drawString(
+						g,
+						"ENEMIES DESTROYED   " + EnemyBulletCollision.destroyed,
+						20, 30);
+
+	}
+
+}
---
+++ src/DemoGameLevel/MyOwnGameLevelUpdate.java
@@ -0,0 +1,51 @@
+package DemoGameLevel;
+
+import java.util.List;
+
+import load.Load;
+import element.Fighter;
+import gameLevel.GameLevelUpdate;
+public class MyOwnGameLevelUpdate extends GameLevelUpdate {
+	
+	public List<List<Object>> list = Load.list;
+
+	public MyOwnGameLevelUpdate(MyOwnGameLevel gl) {
+		super(gl);
+	}
+    
+	
+	
+	public void keyUpdate(long elapsedTime, Fighter fighter) {
+		
+	}
+
+
+	public void playFieldUpdate(long elapsedTime) {
+	}
+
+	public void fighterUpdate(long elapsedTime) {
+		
+	}
+
+	public void cannonUpdate(long elapsedTime) {
+		
+	}
+
+	public void enemyUpdate(long elapsedTime) {
+		
+	}
+
+	public void bonusUpdate(long elapsedTime) {
+		
+	}
+
+
+	public void levelComplete() {
+	}
+
+	
+	public void gameUpdate() {
+		
+	}
+
+}
