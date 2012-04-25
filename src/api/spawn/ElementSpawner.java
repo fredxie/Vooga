b573ae37d4adadc6e6780bd31e4666e4e555090f
Gang Song <gang.song@duke.edu>
2012-04-25 13:59:08 -0400

    Maded some minor changes about spawn

--- src/api/spawn/ElementSpawner.java
+++ src/api/spawn/ElementSpawner.java
@@ -20,8 +20,6 @@ public class ElementSpawner<E extends Element> {
 	private E myElement;
 	private int myElementNum;
 
-	// private PlayField myPlayField;
-
 	/**
 	 * @param element
 	 *            is the type of the element that is going to be spawned
@@ -34,6 +32,10 @@ public class ElementSpawner<E extends Element> {
 		myElement = element;
 	}
 
+	/**
+	 * 
+	 * @return a list of Element E
+	 */
 	public List<E> spawn() {
 
 		List<E> elements = new ArrayList<E>();
@@ -65,7 +67,7 @@ public class ElementSpawner<E extends Element> {
 
 	/**
 	 * @param enemy
-	 *            : change Enemy type to enemy
+	 *            : change Element sub-type to element
 	 */
 	public void setElement(E element) {
 		myElement = element;
@@ -74,51 +76,5 @@ public class ElementSpawner<E extends Element> {
 	public void setElementNum(int num) {
 		myElementNum = num;
 	}
-
-	/**
-	 * 
-	 * @param elapsedTime
-	 * @Description: This method is mainly used for time related spawning
-	 *               behavior, such as SpawnByTime
-	 */
-
-	//
-	// public Enemy[] refresh(long elapsedTime){
-	// if(myTimer.action(elapsedTime)){
-	// myTimer.refresh();
-	// return spawn();
-	// }
-	// return null;
-	// }
-	//
-
-	// public boolean refresh(long elapsedTime){
-	// boolean state=myTimer.action(elapsedTime);
-	// if(state){
-	// myTimer.refresh();
-	// }
-	// return state;
-	// }
-
-	//
-	// public void setTimer(Timer time){
-	// myTimer=time;
-	// }
-
-	// public Enemy[] spawnByFighterState(){
-	// if (hasSpawned == false) {
-	//
-	// Enemy[] retEne=spawn();
-	// if(retEne!=null){
-	// hasSpawned = true;
-	// }
-	// return retEne;
-	// }
-	// return null;
-	// }
-	//
-	// public boolean checkSpawn(){
-	// return hasSpawned;
-	// }
 
 }
