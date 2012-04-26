f224b77ce59023e07b77bd0119a132132ce3e1d1
Ran Zhang <rz22@duke.edu>
2012-04-25 22:39:09 -0400

    hud

--- src/api/hud/Display.java
+++
@@ -1,9 +0,0 @@
-package api.HUD;
-
-import java.awt.Graphics2D;
-
-import com.golden.gamedev.object.GameFontManager;
-
-public abstract class Display {
-	abstract public void display(Graphics2D g, GameFontManager fontManager, String font, String title, int x, int y);
-}
